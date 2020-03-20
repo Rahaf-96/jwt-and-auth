6249d0cdbb818ac732b5fa892a57eb6cbf35975b
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-03-21 01:34:47 +0200

    404 and 500 pages

--- src/controllers/errors.js
+++ src/controllers/errors.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 const notFound = (req, res) => {
-	res.status(404).send('404 Page Not Found');
+	res
+		.status(404)
+		.sendFile(path.join(__dirname, '..', '..', 'public', '404.html'));
 };
 
 const serverError = (error, req, res, next) => {
-	res.status(500).send('Server Error');
+	res
+		.status(500)
+		.sendFile(path.join(__dirname, '..', '..', 'public', '500.html'));
 };
 
 module.exports = { notFound, serverError };
